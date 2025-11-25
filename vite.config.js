9eb2313473cc481e22a6f5e4c831e0e6f5033d86
Takakuraakira-dev <akira10262014@gmail.com>
2025-11-25 21:46:14 +0900

    再ビルドしてGitHub Pages用に更新

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/recipe-app-2025/',  // ← ここ!! 必ずスラッシュで挟む
+  base: '/recipe-app-2025/', // ←ここ！
 })
+
+
