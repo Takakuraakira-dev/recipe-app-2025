32cbfa1ed70b9979929bd27c01af6624224b08f7
Takakuraakira-dev <akira10262014@gmail.com>
2025-11-29 11:41:24 +0900

    fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/recipe-app-2025/',   // ★ ここが超重要！！
   plugins: [react()],
-  base: '/recipe-app-2025/', // ←ここ！
-})
-
+});
 
